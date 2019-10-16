2bd89b05305aa3abb6e06dae3daad489a656af81
Mateusz Mazurkiewicz <imaru89@gmail.com>
2019-10-16 09:50:09 +0200

    sample data

---
+++ consumer/src/main/java/com/dev/life/rsocket/consumer/ConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.dev.life.rsocket.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+}
---
+++ producer/src/test/java/com/dev/life/rsocket/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.life.rsocket.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
